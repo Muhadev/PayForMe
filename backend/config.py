66d6e97f82737799acdf75c2970b7b83e6ca1687
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-08-26 13:21:48 -0700

    Restored database settings

--- backend/config.py
+++ backend/config.py
@@ -13,23 +13,3 @@ class Config:
     GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
     GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
     FRONTEND_URL = os.environ.get('FRONTEND_URL')  # React app URL
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
-
-class TestingConfig(Config):
-    TESTING = True
-    WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-
-class ProductionConfig(Config):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-
-Config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig
-}
